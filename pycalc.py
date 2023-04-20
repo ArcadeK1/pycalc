55785cb6e38c4e675da278c9d46b9d64a0297240
ArcadeK <kgames266@gmail.com>
2023-04-20 12:17:08 +0500

    Added pycalc.py

---
+++ pycalc.py
@@ -0,0 +1,21 @@
+def calc (a, action, b):
+    result = 0
+    if action == '*':
+        result += a*b;
+    elif action == '+':
+        result += a+b;
+    elif action == '-':
+        result += a-b;
+    else:
+        raise ValueError ("Допускается только действие + ; = ; или * .")
+    return result;
+
+
+
+n = int(input("Введите число 1: "))
+
+action = input("Введите действие: ")
+
+m = int(input("Введите число 2: "))
+
+print (f"{n}{action}{m}=", calc (n, action, m))
